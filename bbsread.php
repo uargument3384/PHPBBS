7d4b8f701cb7072c6950cf858c1c74f297ab9f4a
ぽんこつとんこつ <zuotengyangyin0+1@gmail.com>
2026-02-04 21:22:02 +0900

    bbsread.php を更新

--- bbsread.php
+++ bbsread.php
@@ -1,6 +1,6 @@
 <?php
 
-// Coptright 2025 ppmk8player This is Message By Not Delete
+// Coptright 2025-2026 satohina This is Message By Not Delete
 function h($s){ return htmlspecialchars($s, ENT_QUOTES, 'UTF-8'); }
 $data_dir = __DIR__ . '/data';
 ?>
